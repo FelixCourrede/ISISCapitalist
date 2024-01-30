4afbf4239e7d81bfd29f60154da0a2b6d395d118
Louna_Bashti <bashlodu59@orange.fr>
2024-01-30 09:15:02 +0100

    correction des bugs sur les fichiers app et product, initialisation basique des produits dans le app.component.html (app bugguée)

--- Frontend/isis-Frontend/src/app/Graphrequests.ts
+++ Frontend/isis-Frontend/src/app/Graphrequests.ts
@@ -1,60 +1,28 @@
-import {gql} from "@urql/core"
+import {gql} from "@urql/core";
 
-const GET_WORLD = gql`
-query getWorld {
-    id
-    getWorld {
-      activeangels
-      allunlocks {
-        idcible
-        logo
-        name
-        ratio
-        seuil
-        typeratio
-        unlocked
-      }
-      angelbonus
-      angelupgrades {
-        idcible
-        logo
-        name
-        ratio
-        seuil
-        unlocked
-        typeratio
-      }
-      lastupdate
-      logo
-      money
+export const GET_WORLD = gql`
+query ExampleQuery {
+  getWorld {
+    name
+    logo
+    money
+    score
+    totalangels
+    activeangels
+    angelbonus
+    lastupdate
+    products {
+      id
       name
-      score
-      totalangels
-      managers {
-        name
-        logo
-        seuil
-        idcible
-        ratio
-        typeratio
-        unlocked
-      }
-      products {
-        id
-        name
-        logo
-        cout
-        croissance
-        revenu
-        vitesse
-        quantite
-        timeleft
-        managerUnlocked
-        paliers {
-          
-        }
-      }
-      upgrades {
+      logo
+      cout
+      croissance
+      revenu
+      vitesse
+      quantite
+      timeleft
+      managerUnlocked
+      paliers {
         name
         logo
         seuil
@@ -64,7 +32,41 @@ query getWorld {
         unlocked
       }
     }
+    allunlocks {
+      name
+      logo
+      seuil
+      idcible
+      ratio
+      typeratio
+      unlocked
+    }
+    upgrades {
+      name
+      logo
+      seuil
+      idcible
+      ratio
+      typeratio
+      unlocked
+    }
+    angelupgrades {
+      name
+      logo
+      seuil
+      idcible
+      ratio
+      typeratio
+      unlocked
+    }
+    managers {
+      name
+      logo
+      seuil
+      idcible
+      ratio
+      typeratio
+      unlocked
+    }
   }
-  
-
-`
+}`
--- Frontend/isis-Frontend/src/app/app.component.ts
+++ Frontend/isis-Frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WebserviceService } from './webservice.service';
+import { World } from './world';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'isis-Frontend';
+
+  world = new World();
+  constructor(private service: WebserviceService) {
+    service.getWorld().then(
+      world=> {
+        this.world = world.data.getWorld;
+      }
+    );
+  }
 }
