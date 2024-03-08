5c079abd684276fa07d9d0b64f9866d7929fbb16
Louna_Bashti <bashlodu59@orange.fr>
2024-03-08 17:22:28 +0100

    product progressbar avec problèmes

--- Frontend/isis-Frontend/src/app/product/product.component.ts
+++ Frontend/isis-Frontend/src/app/product/product.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Product } from '../world';
-import {MatProgressBarModule} from '@angular/material/progress-bar'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
 import { BigvaluePipe } from '../bigvalue.pipe';
+import { NONE_TYPE } from '@angular/compiler';
 
 
 @Component({
@@ -24,8 +25,32 @@ export class ProductComponent implements OnChanges {
     }
   }
 
+  ngOnInit() {
+    setInterval(() => { this.calcScore(); }, 100);
+  }
+
+  calcScore() {
+    if (timeleft == 0) { return NONE_TYPE }
+    else {
+      timeleft = Date.now() - this.lastupdate;
+      if (timeleft <= 0) {
+        timeleft = 0;
+        thisprogressbarvalue = 0
+      }
+      else {
+        this.progressbarvalue = ((this.product.vitesse -
+          this.product.timeleft) / this.product.vitesse) * 100
+      }
+    }
+
+  }
+
   @Input()
   money: number = 0
+
+  @Output()
+  notifyProduction: EventEmitter<Product> = new
+    EventEmitter<Product>();
 
 
 }
